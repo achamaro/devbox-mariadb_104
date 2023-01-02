eaf417bfb886f2826773daf1d7e51244bf212cbc
achamaro <a.achamaro@gmail.com>
2023-01-02 11:21:32 +0900

    feat: mariadb 10.4

---
+++ README.md
@@ -0,0 +1,13 @@
+# Devbox Mariadb 10.4
+
+## 環境変数
+
+```bash
+MYSQL_PORT:-3306 # MySqlポート
+```
+
+## 起動
+
+```
+devbox shell
+```
---
+++ devbox.d/mariadb/init.sh
@@ -0,0 +1,5 @@
+#! bash
+
+source $PWD/devbox.d/mariadb/set-env.sh
+$PWD/devbox.d/mariadb/start.sh
+source $PWD/devbox.d/mariadb/set-exit.sh
